49beff1aa5ed3fe8064d9adbb1e4df53928e5ccb
Adrian Bryant <bryant.adrian94@gmail.com>
2023-06-30 17:14:30 -0500

    All done

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -2,7 +2,7 @@ console.log('***** Object Practice *****')
 
 // Add the required code to complete the tasks below
 
-/* 1. Create an object literal to represent you!
+/* 1. Create an object literal to represet you!
   - Give the object a 'firstName & (separate) 'lastName' property
   - Give it a boolean 'hasSiblings' property
   - Give it  a numeric 'shoeCount' property
@@ -10,31 +10,42 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: "Adrian",
+  lastName: "Bryant",
+  hasSiblings: true,
+  shoeCount: 16,
+  favThreeFoods: ["Salmon", "Mac & Cheese", "Pizza"]
 };
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
-
+let fullName = me.firstName + " " + me.lastName
+console.log(fullName);
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
-
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[2]);
 
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
+console.log(me.shoeCount);
+me.shoeCount += 1
+console.log(me.shoeCount);
+
 
 
 /* 5. Add a new property to an existing object.
@@ -43,3 +54,5 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favortieColor = 0
+console.log(me);
